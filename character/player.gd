ca5ab1ffa83d50ef00415362783689732e3de122
niniren01 <mgcbok@163.com>
2025-03-13 18:19:01 +0800

    add quit restart fullscreen input map

--- character/player.gd
+++ character/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody3D
 @export var mouse_sensitivity_y = 0.15
 
 func _ready() -> void:
+	#pass
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event: InputEvent) -> void:
@@ -13,6 +14,16 @@ func _input(event: InputEvent) -> void:
 		rotation_degrees.y -= event.relative.x * mouse_sensitivity_h
 		camera_3d.rotation_degrees.x -= event.relative.y * mouse_sensitivity_y
 		camera_3d.rotation_degrees.x = clamp(camera_3d.rotation_degrees.x, -90, 90)
-	
-	if event.is_action_pressed("quit"):
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("quit"):
 		get_tree().quit()
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("fullscreen"):
+		var fs = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+		if fs:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
